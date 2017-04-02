29038c591c5151638162aca9a2a12b9dd9f88b60
Anh Huynh <anh93@drexel.edu>
2017-04-02 03:54:07 -0400

    Update + combine views

--- www/www/js/app.js
+++ www/www/js/app.js
@@ -20,7 +20,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
