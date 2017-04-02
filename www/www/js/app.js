e9a09b4496ce7f39ae3fe54f9105590fcb30ccc3
Ryan Efendy <ryanefendy95@gmail.com>
2017-04-02 11:55:21 -0400

    reformat code & styling

--- www/www/js/app.js
+++ www/www/js/app.js
@@ -5,31 +5,29 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives','firebase'])
-.config(function($ionicConfigProvider) {
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'firebase'])
+  .config(function ($ionicConfigProvider) {
     //Added config
     //$ionicConfigProvider.views.maxCache(5);
     $ionicConfigProvider.scrolling.jsScrolling(false);
     $ionicConfigProvider.tabs.position('bottom'); // other values: top
-})
-.run(function($ionicPlatform,$rootScope) {
+  })
+  .run(function ($ionicPlatform, $rootScope) {
     // $root.extras is used for hiding user features, ie cart icon, menu drawer, navigation icon etc.
     $rootScope.extras = false;
 
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
 
 
-
-
-})
+  })
